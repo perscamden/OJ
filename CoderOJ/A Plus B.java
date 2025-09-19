90cc84915d207fc9a3d776de29db87be19f74212
perscamden <perscamden@gmail.com>
2025-09-19 16:14:08 +0100

    Create A Plus B.java

---
+++ CoderOJ/A Plus B.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+
+
+
+
+class APlusB
+{
+    
+    public static
+    void
+    main
+    (
+        String[] args
+    )
+    {
+        Scanner read = new Scanner( System.in );
+        int A = read.nextInt();
+        int B = read.nextInt();
+        
+        
+        System.out.println( A + B );
+    }
+    
+}
