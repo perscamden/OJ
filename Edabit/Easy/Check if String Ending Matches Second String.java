b177cd1a708c9535bd34580a40ff01be47aee1a5
perscamden <perscamden@gmail.com>
2025-09-21 16:29:45 +0100

    Create Check if String Ending Matches Second String.java

---
+++ Edabit/Easy/Check if String Ending Matches Second String.java
@@ -0,0 +1,15 @@
+class Challenge
+{
+    
+    static
+    boolean
+    checkEnding
+    (
+        String str1
+        , String str2
+    )
+    {
+        return str1.endsWith( str2 );
+    }
+    
+}
