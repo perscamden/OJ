800315a3b76ac46c4ed52d6c0840d9a33587e89e
perscamden <perscamden@gmail.com>
2025-09-22 15:09:15 +0100

    Create Rectangle Area.java

---
+++ CoderOJ/Rectangle Area.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+
+
+
+public class Main
+{
+    
+    public static
+    void
+    main
+    (
+        String args[]
+    )
+    {
+        Scanner read = new Scanner( System.in );
+        double height = read.nextDouble();
+        double weight = read.nextDouble();
+        
+        
+        System.out.println( height * weight );
+    }
+    
+}
