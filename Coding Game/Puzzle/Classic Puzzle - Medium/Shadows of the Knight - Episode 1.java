db84c471e652b71c11420aa619a3781e981fc6aa
perscamden <perscamden@gmail.com>
2025-09-23 15:44:19 +0100

    Update and rename unfinsihed: Shadows of the Knight - Episode 1.java to Shadows of the Knight - Episode 1.java

--- Coding Game/Puzzle/Classic Puzzle - Medium/Shadows of the Knight - Episode 1.java
+++ Coding Game/Puzzle/Classic Puzzle - Medium/Shadows of the Knight - Episode 1.java
@@ -4,9 +4,10 @@ import java.util.*;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+
 class Player
 {
-
+    
     public static
     void
     main
@@ -22,8 +23,8 @@ class Player
         int Y0 = in.nextInt();
         int x = X0;
         int y = Y0;
-        int hiX = H - 1;
-        int hiY = W - 1;
+        int hiX = W - 1;
+        int hiY = H - 1;
         int lowX = 0;
         int lowY = 0;
         
@@ -38,52 +39,65 @@ class Player
             
             switch ( bombDir.charAt( 0 ) )
             {
-
+                
                 case 'U':
-                    hiX = x;
-                    x = ( x + lowX ) / 2;
-
-                case 'D':
-                    x = ( x + hiX + 1 ) / 2;
-
-                case 'R':
-                    y = ( y + hiY + 1 ) / 2;
-
-                case 'L':
+                    hiY = y - 1;
                     y = ( y + lowY ) / 2;
-
+                    
+                    break;
+                    
+                case 'D':
+                    lowY = y + 1;
+                    y = ( y + hiY + 1 ) / 2;
+                    
+                    break;
+                    
+                case 'R':
+                    lowX = x + 1;
+                    x = ( x + hiX + 1 ) / 2;
+                    
+                    break;
+                    
+                case 'L':
+                    hiX = x - 1;
+                    x = ( x + lowX ) / 2;
+                    
+                    break;
+                    
             }
-
-
+            
+            
             if
             (
                 bombDir.length() > 1
             )
             {
-
+                
                 if
                 (
                     bombDir.charAt( 1 ) == 'R'
                 )
                 {
-                    y = ( y + hiY + 1 ) / 2;
+                    lowX = x + 1;
+                    x = ( x + hiX + 1 ) / 2;
                 }
                 else
                 {
-                    y = ( y + lowY ) / 2;
+                    hiX = x - 1;
+                    x = ( x + lowX ) / 2;
                 }
-
+                
             }
             
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
-
-
+            
+            
             // the location of the next window Batman should jump to.
             System.out.println( x + " " + y );
         }
-
-
+        
+        
     }
-
+    
 }
