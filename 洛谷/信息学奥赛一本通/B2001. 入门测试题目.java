0bf7d720b2304cf0e1733d78297e7aa62acc15fa
perscamden <perscamden@gmail.com>
2025-09-24 14:37:12 +0100

    Create B2001. 入门测试题目.java

---
+++ 洛谷/信息学奥赛一本通/B2001. 入门测试题目.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+
+
+
+
+class Main
+{
+    
+    public static
+    void
+    main
+    (
+        String[] args
+    )
+    {
+        Scanner read = new Scanner( System.in );
+        long a = read.nextLong();
+        long b = read.nextLong();
+        
+        System.out.println( a + b );
+    }
+    
+}
