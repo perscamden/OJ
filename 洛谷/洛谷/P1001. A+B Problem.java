3792db5a56efd82504d99cb2283eaec917d9d30d
perscamden <perscamden@gmail.com>
2025-09-24 14:02:49 +0100

    Create P1001. A+B Problem.java

---
+++ 洛谷/洛谷/P1001. A+B Problem.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+
+
+
+
+class Main
+{
+    
+    public static
+    void
+    main
+    (
+        String[] args
+    )
+    {
+        Scanner read = new Scanner( System.in );
+        int a = read.nextInt();
+        int b = read.nextInt();
+        
+        System.out.println( a + b );
+    }
+    
+}
