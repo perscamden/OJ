e2a234e612c78e3ca1b80335cac1300543a3ea34
perscamden <perscamden@gmail.com>
2025-09-27 15:15:06 +0100

    Create Shift the liquid.java

---
+++ CoderOJ/Shift the liquid.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+
+
+
+public class Main
+{
+    
+    public static
+    void
+    main
+    (
+        String args[]
+    )
+    {
+        Scanner read = new Scanner( System.in );
+        double A = read.nextDouble();
+        int B = read.nextInt();
+        
+        
+        System.out.println( ( int ) ( Math.ceil(  A / B ) ) );
+    }
+    
+}
