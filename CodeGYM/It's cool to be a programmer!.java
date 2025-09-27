1891ed7ef7cbad55c4a2fc63a2357bd74620d6c8
perscamden <perscamden@gmail.com>
2025-09-27 09:02:10 +0100

    Create It's cool to be a programmer!.java

---
+++ CodeGYM/It's cool to be a programmer!.java
@@ -0,0 +1,20 @@
+package en.codegym.task.jdk13.task01.task0101;
+
+/* 
+It's cool to be a programmer!
+*/
+
+public class Solution
+{
+    
+    public static
+    void
+    main
+    (
+        String[] args
+    )
+    {
+        System.out.println(  "I think being a programmer is cool" );
+    }
+    
+}
