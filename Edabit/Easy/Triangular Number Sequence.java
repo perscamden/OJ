8db69c9a7608e134a841d6882cd669d9e26e692c
perscamden <perscamden@gmail.com>
2025-09-27 09:49:26 +0100

    Create Triangular Number Sequence.java

---
+++ Edabit/Easy/Triangular Number Sequence.java
@@ -0,0 +1,27 @@
+public class Challenge
+{
+      
+      public static
+      int
+      triangle
+      (
+          int n
+      )
+      {
+          int answer = 0;
+          
+          
+          for
+          (
+              int i = 1
+              ; i <= n
+              ; i ++
+          )
+          {
+             answer += i;
+          }
+            
+            return answer;
+    }
+      
+}
